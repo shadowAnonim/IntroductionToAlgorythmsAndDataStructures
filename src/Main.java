9f09e7746eb24511c32db9275d48532ddaaf5e02
Аноним Евгений <shadow.anonim@yandex.ru>
2024-09-14 10:07:24 +0300

    Task 2 completed

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,17 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Первые 10 чисел гармонического ряда:");
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.printf("%.3f\n", (float)1/i);
+        }
+
         Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
         while (sc.hasNextInt())
         {
-            System.out.println(fact(sc.nextInt()));
+            System.out.println("Факториал числа: " + fact(sc.nextInt()));
         }
     }
 
